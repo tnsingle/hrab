ccccf02c96c94e0cb8b69288db86af906c07e3e8
Nikki Singletary <nikki@tacobunbun.com>
2025-03-02 21:13:21 -0600

    import fixes

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import Collapse from "@kunukn/react-collapse";
+import { Collapse } from "@kunukn/react-collapse";
 
 export default class Timeline extends React.Component {
 
